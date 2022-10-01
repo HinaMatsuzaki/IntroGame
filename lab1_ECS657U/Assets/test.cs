7ad9c2374dbee82b906bd7a91854215c31980a9b
HinaMatsuzaki <hmatsuzaki@knox.edu>
2022-10-01 15:46:41 +0100

    Copy Unity project
    Copy my Unity project into the folder where I cloned the repository

---
+++ lab1_ECS657U/Assets/test.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    public int force = 500;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+           m_Rigidbody.AddForce(transform.up * force);
+    }
+}
